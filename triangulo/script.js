338652f544c39e089aef973d2b8b428645669913
Vieyra <vieyraurenabravo@gmail.com>
2020-06-17 22:28:35 -0600

    Agrego ejercico del triángulo

---
+++ triangulo/script.js
@@ -0,0 +1,13 @@
+// formar triangulo con asteriscos
+//crear una variable asterisco
+//crear la variable para while para el conteo
+//hacer un while
+
+let asterisco = '';
+let a = 1;
+
+while (a < 8) {
+  asterisco=  asterisco + '*';
+  a = a + 1;
+  console.log(asterisco)
+}
